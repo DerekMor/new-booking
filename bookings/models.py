e60a18ac699b31fbbf05eb47d1cf109fb6ec3b47
DerekMor <>
2023-08-29 21:52:14 +0000

    Create model and make migrations

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    canceled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date} {self.time}"
