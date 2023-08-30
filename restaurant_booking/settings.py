3e9fbcdff4d289cad6ff813d1441ebcce9fae3c3
DerekMor <derekmoriarty24@gmail.com>
2023-08-30 22:07:59 +0000

    attempt to customise allauth login page and fix csrf error. debug still required

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -130,3 +130,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-derekmor-newbooking-dqsvk2fptl1.ws-eu104.gitpod.io'
+]
+
+LOGIN_REDIRECT_URL = 'booking_list' 
+ACCOUNT_SIGNUP_REDIRECT_URL = 'booking_list'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
