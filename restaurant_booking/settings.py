bf1b93f4cc6caf1b92f63de3dc19219f9447b575
DerekMor <derekmoriarty24@gmail.com>
2023-09-04 22:38:51 +0000

    static files fix attempt

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "https://res.cloudinary.com/drl0xmt36/static/"
+STATIC_URL = "static/"
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'https://res.cloudinary.com/drl0xmt36/media/'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
